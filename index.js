d5d2be69d06ef917cb47f5c173a14935ebed9670
Bhashwar <bhaswarsengupta@gmail.com>
2023-03-29 15:03:39 +0530

    completed

--- index.js
+++ index.js
@@ -30,3 +30,48 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+app.route('/api/:date?').get((req, res) => {
+
+    //Get the user input
+    const input = req.params.date;
+    // console.log(input);
+
+    //Check whether user gave any input. If not, return current date.
+    if (input === undefined) {
+        const utc = new Date().toUTCString();
+        const unix = Date.parse(utc);
+        res.json({"unix": unix, "utc": utc});
+    }
+
+    //Parse the input into a Date object
+    let parsedDate = '';
+
+    //Detect a unix timestamp
+
+    //Count the number of decimal points in the input
+    const numDecimals = input.match(/\./g);
+    const numAddOperator = input.match(/\+/g);
+    const numSubOperator = input.match(/-/g);
+    const regex = /[^0-9\+-\.]/g;
+    const condition1 = (numDecimals === null ? true : numDecimals.length === 1 ? true : false);
+    const condition2 = (numAddOperator === null ? true : (numAddOperator.length === 1 && input[0] === '+') ? true : false);
+    const condition3 = (numSubOperator === null ? true : (numSubOperator.length === 1 && input[0] === '-') ? true : false);
+
+    if (condition1 && condition2 && condition3 && !regex.test(input)) {
+        parsedDate = new Date(parseInt(input));
+    } else {
+        parsedDate = new Date(input);
+    }
+
+    //Catch the invalid date
+    if (parsedDate.toString() === "Invalid Date") {
+        res.json({"error": "Invalid Date"});
+    }
+
+    //Serve JSON with correct unix timestamp and utc date.
+    const utcDate = parsedDate.toUTCString();
+    const unixTime = Date.parse(utcDate);
+    res.json({"unix": unixTime, "utc": utcDate});
+});
